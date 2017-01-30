9179dc97cb0aedca154de86b1bfa68890143d5d0
Jennifer Beck <jjbeck@yahoo-inc.com>
2017-01-30 10:10:28 -0800

    Initial Commit

---
+++ TwitterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterDemo
+//
+//  Created by Jennifer Beck on 1/30/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
